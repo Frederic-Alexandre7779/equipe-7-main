085c0c9ef35bb8341121e67e0315b319fe355c96
Frédéric-Alexandre Caouette <FACAO@ulaval.ca>
2025-03-29 15:07:40 -0400

    question1 fini

--- question1.py
+++ question1.py
@@ -2,21 +2,97 @@
 #méthode de la relaxation pour calculer le potentiel en tout point
 #1ere à 0V, 2e à 100V etc
 #Trouver un critère d'arrêt dans le calcul
-import matplotlib as mpl
-import math
+import matplotlib.pyplot as plt
 import numpy as np
 
-def construire_pm(N, a, b, c, d, e, f):
-    dimensions_pm = (2*a + (N+1)c/2 + (N-1)d/2 ,f)
-    pot_dyn = []
-    for i in range(1, N+1):
-        pot_dyn.append(i*100)
 
-    res = []
-    print(pot_dyn)
-    return res
-    
+# ------------- CHANGER LES PARAMÈTRES AVANT DE LANCER LE CODE -------------------- # 
+#initialiser la géométrie
+a, b, c, d, e, f = 3, 2, 4, 2, 0.2, 6
+N = 5
+dx = 0.1 # step
 
 
+largeur = (2*a + (N+1)*(c/2) + (N-1)*(d/2)) # trouvé en analysant l'image 
+Lx,Ly = largeur, f # grandeur de la grille en x et en y sans espacement autour du pm
+nx, ny = int(Lx/dx), int(Ly/dx) # nombre de points en x et en y
 
-print(construire_pm(N=4, a=3, b=2, c=4, d=2, e=0.2, f=6))
+x = np.linspace(0, Lx, nx)
+y = np.linspace(0, Ly, ny)
+X, Y = np.meshgrid(x, y)
+
+#initialiser le potentiel à 0 partout
+V = np.zeros((ny,nx)) # potentiel initial
+bloqué = np.zeros((ny,nx), dtype=bool)
+
+
+# rentrer les dynodes pour laisser un potentiel qui ne varie pas
+# Cette fonction est en [y,x]
+#f, b et e sont en x
+# a,c,d sont en y
+def placer_dynodes_bas(V, bloqué):
+    for i in range(N//2 + N%2):
+        pot_dyn = (2*i+1) * 100
+        vert_start = f-b #coordonnée verticale du début de la dynode
+        vert_end = vert_start + e # la fin
+        horiz_start = a + i*(c+d)   #coordonnée horizontale du début de la dynode
+        horiz_end = horiz_start + c # fin
+
+        ix_start = int(horiz_start/dx) #donne l'indice de colonne correspondant sur la grille
+        ix_end = int(horiz_end/dx) # même chose mais pour la fin
+        iy_start = int(vert_start/dx) # début y
+        iy_end = int(vert_end/dx) # fin y
+
+        V[iy_start:iy_end, ix_start:ix_end] = pot_dyn
+        bloqué[iy_start:iy_end, ix_start:ix_end] = True
+    return V, bloqué
+
+def placer_dynodes_haut(V, bloqué):
+    for i in range(N//2):
+        pot_dyn = (2*(i+1)) * 100
+        vert_start = b #coordonnée verticale du début de la dynode
+        vert_end = b + e # la fin
+        horiz_start = a + (i+1)*c +d/2 + i*d - c/2 #coordonnée horizontale du début de la dynode
+        horiz_end = horiz_start + c # fin
+
+        ix_start = int(horiz_start/dx) #donne l'indice de colonne correspondant sur la grille
+        ix_end = int(horiz_end/dx) # même chose mais pour la fin
+        iy_start = int(vert_start/dx) # début y
+        iy_end = int(vert_end/dx) # fin y
+
+        V[iy_start:iy_end, ix_start:ix_end] = pot_dyn
+        bloqué[iy_start:iy_end, ix_start:ix_end] = True
+    return V, bloqué
+
+V, bloqué = placer_dynodes_haut(V, bloqué)
+V, bloqué = placer_dynodes_bas(V, bloqué)
+
+# Relaxation aka ce qui n'existe pas en gph sauf si t'es camille et tu bois en criss
+def relaxation(V, bloqué, tolérance=1e-5, max_iter=10000):
+    for iteration in range(max_iter):
+        V_old = V.copy() # pour la comparaison pour la tolérance
+        for i in range(1, ny-1): #tous les points sauf les bords
+            for j in range(1, nx-1): # meme chose en x
+                if not bloqué[i, j]:
+                    V[i, j] = 0.25 * (V_old[i+1, j] + V_old[i-1, j] +
+                                     V_old[i, j+1] + V_old[i, j-1])
+        chang_pot_max = np.max(np.abs(V - V_old))
+        if chang_pot_max < tolérance:
+            print(f"Convergence atteinte après {iteration} itérations avec une tolérance de {tolérance} de différence)")
+            print("Nous avons atteint un potentiel qui ne varie presque plus et le problème est considéré résolu")
+            break
+    else:
+        print("Attention !!!!!!!!!! ")
+        print("Le maximum d'itérations a été atteint sans stabilisation, donc le programme a été arrêté")
+    return V
+
+#Afficher le PM
+cp = plt.contourf(X, Y, V, levels=100, cmap="plasma")
+plt.colorbar(cp, label="Potentiel (V)")
+plt.title("Potentiel électrique dans le tube PM")
+plt.xlabel("x (mm)")
+plt.ylabel("y (mm)")
+plt.axis('equal')
+plt.tight_layout()
+plt.savefig("potentiel_2r_PM.png", dpi=300)
+plt.show()
